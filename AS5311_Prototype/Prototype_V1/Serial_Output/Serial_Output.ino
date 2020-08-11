8c0c528d0f9d9d7a998d1cc9d62a1d69fc1a575d
Bryson Goto <55998145+BGoto808@users.noreply.github.com>
2020-08-11 09:23:51 -1000

    Updated summer prototyping code

--- AS5311_Prototype/Prototype_V1/Serial_Output/Serial_Output.ino
+++ AS5311_Prototype/Prototype_V1/Serial_Output/Serial_Output.ino
@@ -1,36 +1,48 @@
-#define DELAY_IN_SECONDS 5
+#define DELAY_IN_SECONDS 3
 
 #include "AS5311.h"
 
 #define CS 9
 #define CLK A5
 #define DO A4
-#define PULSE 11
+//#define PULSE 11
 
+#define HYPNOS3 5   // Hypnos 3V Rail
+#define HYPNOS5 6   // Hypnos 5V Rail
 
 uint32_t start = 0;
 uint32_t prevTwoSig = 0;
 float elapsed = 0;
 float prev = 0;
 int count = 0;
+
 void setup() {
 
-    pinMode(5, OUTPUT);
-    digitalWrite(5, LOW); // Sets pin 5, the pin with the 3.3V rail, to output and enables the rail
-    pinMode(6, OUTPUT);
-    digitalWrite(6, HIGH); // Sets pin 6, the pin with the 5V rail, to output and enables the rail
-  // put your setup code here, to run once:
+  while(!Serial) {} // Gives user time to put magnet in place before opening Serial Monitor
+
+  pinMode(HYPNOS3, OUTPUT);
+  digitalWrite(HYPNOS3, LOW); // Sets pin 5, the pin with the 3.3V rail, to output and enables the rail
+  pinMode(HYPNOS5, OUTPUT);
+  digitalWrite(HYPNOS5, HIGH); // Sets pin 6, the pin with the 5V rail, to output and enables the rail
 
   // Pins to communicate with AS5311
   pinMode(CS, OUTPUT);
   pinMode(CLK, OUTPUT);
-  pinMode(DO, INPUT_PULLDOWN);
+  pinMode(DO, INPUT_PULLUP);
   digitalWrite(CS, HIGH);
   digitalWrite(CLK, LOW);
 
+  delay(2000);
 
-  // Get starting Serial output (0-4095 value)
-  start = getSerialPosition(CLK, CS, DO);
+  // Takes 16 measurements and averages them for the starting Serial value (0-4095 value)
+  for(int j = 0; j < 16; j++)
+  {
+    start += getSerialPosition(CLK, CS, DO);
+  }
+  start /= 16;
+
+  //  Serial.println("Start: " + String(start));
+
   // Save 2 most significant bits of start
   prevTwoSig = start & 0xC00;
 
@@ -42,36 +54,56 @@ void setup() {
 */
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-//   
   int actualMiliSeconds = (DELAY_IN_SECONDS - 1) * 1000;
   delay(actualMiliSeconds);
 
-//   delay for another 1 sec in total
-    // Note: Sensor will take 16 points over the last 896 seconds
+  // delay for another 1 sec in total
   delay(104);
+
+  // Note: Sensor will take 16 points over the last 896 seconds
   // TODO: Verify how quickly these points are being taken
   // 16 point average of Serial Position
-    // 56ms * 16 = 896ms
-  int average = 0;
+  // 56ms * 16 = 896ms
+
+  int average = 0;   // Average of 16 measurements
+  // int remember = 0;  // Previous measured value
+  // int test = 0;      // Current measured value
+
   for(int j = 0; j < 16; j++)
   {
     average += getSerialPosition(CLK, CS, DO);
+/* // For troubleshooting
+    if (remember == 0) {
+      Serial.println("Difference: " + String(average));
+    }
+    else {
+      test = average - remember;
+      Serial.println("Difference: " + String(test));
+    }
+    remember = average;
+*/
   }
-  average /= 16;
 
+  average /= 16;
+  // Serial.println("Average: " + String(average));
 
   // Also updates prevTwoSig to two most significant bits of first param, is being passed by ref
+  // Verifies that reading is somewhat accurate
   elapsed = computeElapsed(average, prevTwoSig, elapsed);
+  // Serial.println("Elapsed: " + String(elapsed));
 
   // Computes total distance
-//   Serial.println("current disp: " + String(((2.0 * ((int) average - (int) start))/4095.0)));
-//   Serial.println("current elapsed: " + String(elapsed));
+  float distance = (elapsed + ((2.0 * ((int) average - (int) start))/4095.0));               // Distance in millimeters
+  float distanceMicro = (elapsed * 1000) + ((2000 * ((int) average - (int) start))/4095.0);  // Distance in micrometers
+  float difference = 0;                                                                      // Millimeters since last measurement
+
+  // Reads the movement if any, else it sets the changed distance to 0
+  if (distance != prev) {
+    difference = distance - prev;
+  }
+
   Serial.println("=============== " + String(count) + " ===============");
-  float distance = (elapsed + ((2.0 * ((int) average - (int) start))/4095.0));
-  float distanceMicro = (elapsed * 1000) + ((2000 * ((int) average - (int) start))/4095.0);
-  float difference = distance - prev;
   Serial.println("Changed Distance: " + String(difference) + "mm");
   Serial.println("Total distance: " + String(distance) + "mm");
   Serial.println("Total distance um: " + String(distanceMicro) + "um");
