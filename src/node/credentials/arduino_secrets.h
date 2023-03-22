f335786c4b7f4de85474e797d470a2a7cdf3c9a3
solidstatebird <33707046+solidstatebird@users.noreply.github.com>
2023-03-22 15:53:10 -0700

    change database name

--- src/node/credentials/arduino_secrets.h
+++ src/node/credentials/arduino_secrets.h
@@ -1,12 +1,12 @@
 //change to match desoired wifi network
-#define SECRET_SSID ""
-#define SECRET_PASS ""
+#define SECRET_SSID "dend"
+#define SECRET_PASS "12345678"
 
 //set the database name
-#define MQTT_DATABASE "Dendrometer(V4)"
+#define MQTT_DATABASE "UBC_Dendrometer"
 
 //don't modify this
-#define SECRET_BROKER ""
+#define SECRET_BROKER "cas-mosquitto.biossys.oregonstate.edu"
 #define SECRET_PORT 1883
 #define BROKER_USER ""
 #define BROKER_PASS ""
