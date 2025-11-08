e8f4500581315e5dbf9f277d0b1e4cc3c5fc5291
Nicholas Le <131207716+nicholasqle@users.noreply.github.com>
2025-11-07 16:16:52 -0800

    generic credentials

---
+++ src WiFi/node/arduino_secrets.h
@@ -0,0 +1,13 @@
+//change to match desired wifi network
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+//set the database name
+#define MQTT_DATABASE ""
+
+//don't modify this
+#define SECRET_BROKER ""
+#define SECRET_PORT 1883
+#define BROKER_USER ""
+#define BROKER_PASS ""
+#define PROJECT ""
